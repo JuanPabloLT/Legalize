df221e16bd56e7256355c7db83d2c88830e960b4
JUAN PABLO LONDO�O TOB�N <juanlondono236563@correo.itm.edu.co>
2020-04-23 13:56:57 -0500

    Modify ConverterHelper with Trip detail ExpenseType

--- Legalize.Web/Helpers/IConverterHelper.cs
+++ Legalize.Web/Helpers/IConverterHelper.cs
@@ -10,13 +10,14 @@ namespace Legalize.Web.Helpers
 {
     public interface IConverterHelper
     {
+        LegalizeResponse ToLegalizeResponse2(LegalizeEntity legalize);
         LegalizeResponse ToLegalizeResponse(LegalizeEntity legalize);
         TripResponse ToTripResponse(TripEntity trip);
         CityResponse ToCityResponse(CityEntity city);
-
+        UserResponse ToUserResponse(UserEntity user);
         TripEntity ToTipEntity(TripViewModel tripViewModel, string path, bool isNew);
         TripViewModel ToTripViewModel(TripEntity tripEntity);
-
+        ExpenseTypeResponse ToExpenseTypeResponse(ExpenseTypeEntity expense);
 
     }
 }
